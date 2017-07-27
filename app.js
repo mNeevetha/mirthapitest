9bc045e5d1963f1cae83d24697ab8fc71040a5db
Sandeep Kanthappa <Sandeep.Kanthappa@cognizant.com>
2017-07-27 19:05:47 +0530

    added api for pdf2

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var app = express();
 const aps = require('./API');
 const apppdf = require('./apppdf');
 var bodyParser = require('body-parser');
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
+
 // Routes
 app.get('/', function(req, res) {
   res.send('Hello World!');
